401f9da12acea3deece1959ceb0278edc26e0f67
isannni <ikhsaniahmad61@gmail.com>
2025-08-14 15:45:58 +0700

    feat(Celcius-Fahrenheit: Convert Celcius to Fahrenheit

---
+++ Celcius-Fahrenheit.js
@@ -0,0 +1,8 @@
+// Kalkulator Konversi Suhu Sederhana
+
+// Celsius ke Fahrenheit
+function celsiusToFahrenheit(celsius) {
+    return (celsius * 9/5) + 32;
+}
+
+console.log("25°C ke Fahrenheit:", celsiusToFahrenheit(25));
---
+++ Celsius-Kelvin.js
@@ -0,0 +1,6 @@
+// Celsius ke Kelvin
+function celsiusToKelvin(celsius) {
+    return celsius + 273.15;
+}
+
+console.log("25°C ke Kelvin:", celsiusToKelvin(25));
